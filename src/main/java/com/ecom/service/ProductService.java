7b8ac83ebc346590051b4ad1e591bc3615e2b5fb
hvhuy2004 <huygioihoa@gmail.com>
2024-09-22 00:04:55 +0700

    vid9

---
+++ src/main/java/com/ecom/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.ecom.service;
+
+import java.util.List;
+
+import com.ecom.model.Product;
+
+public interface ProductService {
+	
+	public Product saveProduct(Product product);
+	
+	public List<Product> getAllProduct();
+	
+	public Boolean deleteProduct(int id);
+
+}
---
+++ src/main/java/com/ecom/service/impl/ProductServiceImpl.java
@@ -0,0 +1,42 @@
+package com.ecom.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import com.ecom.model.Product;
+import com.ecom.repository.ProductRepository;
+import com.ecom.service.ProductService;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+	@Autowired
+	ProductRepository productRepository;
+	
+	@Override
+	public Product saveProduct(Product product) {
+		return productRepository.save(product);
+	}
+
+	@Override
+	public List<Product> getAllProduct() {
+		
+		return productRepository.findAll();
+	}
+
+	@Override
+	public Boolean deleteProduct(int id) {
+		
+		Product product = productRepository.findById(id).orElse(null);
+		if (!ObjectUtils.isEmpty(product)) {
+			productRepository.delete(product);
+			return true;
+		}
+		
+		return false;		
+	}
+
+}
